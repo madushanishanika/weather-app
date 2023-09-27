59fd93c5e1db5a03514c722f60e62beb8f34c53a
madushanishanika <45064135+madushanishanika@users.noreply.github.com>
2023-09-27 22:53:32 +0530

    Add files via upload

---
+++ scripts/weatherapp.js
@@ -0,0 +1,16 @@
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
+  var map = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+  
